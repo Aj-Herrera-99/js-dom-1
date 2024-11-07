c5b9c449336b2736ff4e03efe7df1ff77da3d870
Aj-Herrera-99 <ajherrera999@gmail.com>
2024-11-07 14:28:52 +0100

    bonus

--- script/script.js
+++ script/script.js
@@ -6,15 +6,47 @@ Al click del bottone, la lampadina dovrà accendersi (dovremo quindi utilizzare 
 
  */
 
+//* NORMALE
 // variables
 const img = document.querySelector("img");
 const button = document.querySelector("button");
-// eventListener
+// event listener
 button.addEventListener("click", handleLightBulb);
-
 // event handler
 function handleLightBulb() {
     img.src = "img/yellow_lamp.png";
     img.alt = "lampadina accesa";
     img.style.rotate = "y 180deg";
+}
+
+/**
+Facciamo accendi e spegni:
+- Al primo click la lampadina si accende e nel bottone compare la scritta "Spegni"
+- Al secondo click la lampadina si spegne e nel bottone compare la scritta "Accendi"
+ */
+//* BONUS
+// variables
+const imgBonus = document.getElementById("img-bonus");
+const toggleOnOffBtn = document.getElementById("toggle-on-off-btn");
+// event listeners
+toggleOnOffBtn.addEventListener("click", handleToggleOnOff);
+// event handlers
+function handleToggleOnOff() {
+    console.log(imgBonus.src);
+    if (imgBonus.src.includes("white_lamp.png")) {
+        // cambio immagine lampadina
+        imgBonus.src = "img/yellow_lamp.png";
+        imgBonus.alt = "lampadina accesa bonus";
+        imgBonus.style.rotate = "y 180deg";
+        // cambio testo button
+        toggleOnOffBtn.innerHTML = "Spegni";
+    } 
+    else if(imgBonus.src.includes("yellow_lamp.png")) {
+        // cambio immagine lampadina
+        imgBonus.src = "img/white_lamp.png";
+        imgBonus.alt = "lampadina spenta bonus";
+        imgBonus.removeAttribute("style");
+        // cambio testo button
+        toggleOnOffBtn.innerHTML = "Accendi";
+    }
 }
